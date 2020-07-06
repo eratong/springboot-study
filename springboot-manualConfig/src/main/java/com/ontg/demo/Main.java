071b5b040e58f981d9dd2bce5671ff24dbe1f55c
tong <yetong.xu@counect.com>
2020-07-06 11:20:08 +0800

    初始化

--- springboot-manualConfig/src/main/java/com/ontg/demo/Main.java
+++ springboot-manualConfig/src/main/java/com/ontg/demo/Main.java
@@ -1,4 +1,14 @@
 package com.ontg.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 关于手动配置多数据源redis和db的问题
+ */
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
 }
