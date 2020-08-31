726064a58f121f711ce52977a37e7a80099694e0
tong <yetong.xu@counect.com>
2020-09-01 00:27:19 +0800

    全局变量测试

---
+++ utilTest/src/main/java/com/ontg/demo/service/Test11.java
@@ -0,0 +1,12 @@
+package com.ontg.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Test11 {
+    String test2="test1";
+
+    public void s(){
+        System.out.println("Test11======"+test2);
+    }
+}
---
+++ utilTest/src/main/java/com/ontg/demo/service/Test22.java
@@ -0,0 +1,13 @@
+package com.ontg.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Test22 {
+
+    String test2="test2";
+
+    public void s(){
+        System.out.println("Test22======"+test2);
+    }
+}
